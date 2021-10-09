cfea8a34c952d07c68373aa4e1d39b278fff0d0d
Oliver <otlg123@gmail.com>
2021-10-09 14:30:08 +0100

    Set WiFi power mode to min

--- main/main.c
+++ main/main.c
@@ -94,6 +94,7 @@ void initialiseWifi()
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
     ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
     ESP_ERROR_CHECK(esp_wifi_start());
+    ESP_ERROR_CHECK(esp_wifi_set_ps(WIFI_PS_MIN_MODEM));
 
     // Wifi connection is ongoing
     // Wait for either the connected, or faile bit to be set
