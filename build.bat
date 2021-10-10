0495b4e7e377afa24efb5a6328db85d45fba9941
Oliver <otlg123@gmail.com>
2021-10-10 21:02:58 +0100

    Improve setup bat

--- build.bat
+++ build.bat
@@ -1,8 +1,11 @@
 set PROJECT_DIR=%CD%
-set IDF_PATH=C:\Users\Ollie\Documents\Programs\esp-idf-v4.3.1
+if "%IDF_PATH%" == "" (
+    @echo Please set the IDF_PATH environment variable to point to the ESP-IDF installation
+    exit 1
+)
 
 :: IDF doesn't work well with pyenv so add specific python version to path
-set PPPATH=C:\Users\Ollie\.pyenv\pyenv-win\versions\3.8.1
+set PPPATH=%userprofile%\.pyenv\pyenv-win\versions\3.8.0-amd64
 set PATH=%PPPATH%;%PATH%
 
 cd %IDF_PATH%
@@ -10,8 +13,3 @@ call export.bat
 call cd %PROJECT_DIR%
 
 call idf.py build
-
-@REM mkdir -p build
-@REM cd build
-@REM cmake .. -G Ninja   # or 'Unix Makefiles'
-@REM ninja
--- install_idf.bat
+++ install_idf.bat
@@ -1,10 +1,13 @@
 set PROJECT_DIR=%CD%
-set IDF_PATH=C:\Users\Ollie\Documents\Programs\esp-idf-v4.3.1
+if "%IDF_PATH%" == "" (
+    @echo Please set the IDF_PATH environment variable to point to the ESP-IDF installation
+    exit 1
+)
 
 :: IDF doesn't work well with pyenv so add specific python version to path
-set PPPATH=C:\Users\Ollie\.pyenv\pyenv-win\versions\3.8.1
+set PPPATH=%userprofile%\.pyenv\pyenv-win\versions\3.8.0-amd64
 set PATH=%PPPATH%;%PATH%
-echo %PATH%
+@echo %PATH%
 cd %IDF_PATH%
 call install.bat
 
